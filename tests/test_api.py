c5fe5987ed5ccae4e92778c0fb48750b743bf063
ewerttrewe <93892998+ewerttrewe@users.noreply.github.com>
2024-01-08 16:44:22 +0100

    fix mock
    without mocking post method

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,15 +3,18 @@
 import pytest
 from unittest.mock import patch
 from conftest import client, set_up_database_connection
+import json
+import os
 
 
 class TestUrlShortenerAPI:
     def test_post(self, client, set_up_database_connection):
         dummy_data = {"longUrl": "https://www.facebook.com/"}
+        os.environ["TESTING"] = "True"
         test_db_connection = set_up_database_connection
-        with patch("api.api.connection_db", return_value=test_db_connection):
-            response = client.post("/api/post-org-url", json=dummy_data)
-
+        # with patch("api.api.connection_db", return_value=test_db_connection):
+        #     response = client.post("/api/post-org-url", json=dummy_data)
+        response = client.post("/api/post-org-url", json=dummy_data)
         assert response.status_code == 200
 
     def test_get(self):
